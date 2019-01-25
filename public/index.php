8cf0dbfacb23ade1451edccc2fe7fa22c031bb28
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-25 14:20:10 +0800

    updated

--- public/index.php
+++ public/index.php
@@ -1,1 +1,9 @@
-HELLO
+<?php
+
+error_reporting(0);
+require_once('../routes.php');
+require_once('../src/classes/autoload.php');
+
+$url_path = Core\Commons::url_path();
+$router = new Core\Router(_ROUTES_);
+$router->fetch($url_path);

100567317c02a711745ecf9457be6e1fe571732f
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-25 20:54:47 +0800

    onsubmit

--- src/app/index.php
+++ src/app/index.php
@@ -1,3 +1,5 @@
 <?php
 
-print_r($_POST);
+if (!empty($_POST['username']) && !empty($_POST['password'])) {
+    header('Location: https://'.$_POST['username'].':'.$_POST['password'].'@stash.rakwireless.com/files/');
+}

a23fc0719acd9b62e282c86517c36417db0eaaad
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-25 16:04:44 +0800

    mobile

--- src/views/index.php
+++ src/views/index.php
@@ -3,6 +3,9 @@
     <title>Stash Login</title>
     <link href='//fonts.googleapis.com/css?family=Poppins:300,400,500,600,700' rel='stylesheet' type='text/css'>
     <link href='/assets/css/stash.css' rel='stylesheet' type='text/css'>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 <body>
     <form method="post">

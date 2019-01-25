be065e11e2d32ad23157fa089c025f75c66aee01
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-25 21:52:38 +0800

    error pages

--- public/forbidden.php
+++ public/forbidden.php
@@ -1,6 +1,6 @@
 <?php
 
-setcookie('_AUTH_ERROR_', 'xFA-403', time()+10, '/');
-header('Location: /', true, 303);
+//setcookie('_AUTH_ERROR_', 'xFA-403', time()+10, '/');
+//header('Location: /', true, 303);
 
-echo "Forbidden Access";
+//echo "Forbidden Access";

29d4b0bacd5443cf097253963aa484fd279adbea
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-25 21:37:21 +0800

    error pages

--- src/app/index.php
+++ src/app/index.php
@@ -4,4 +4,4 @@ if (!empty($_POST['username']) && !empty($_POST['password'])) {
     header('Location: https://'.$_POST['username'].':'.$_POST['password'].'@stash.rakwireless.com/files/');
 }
 
-echo $_COOKIE['_AUTH_ERROR_'];
+print_r($_COOKIE['_AUTH_ERROR_']);

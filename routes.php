7bf28f84611eec15db60257937f9170e2046a919
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-25 20:58:33 +0800

    error pages

--- routes.php
+++ routes.php
@@ -5,5 +5,8 @@ const _ROUTES_ = [
     '/signup/' => 'signup.php',
     '/reset/' => 'reset.php',
 
+    
+    '/unauthorized/' => 'error-401.php'
+    '/forbidden/' => 'error-403.php'
     '{PAGE_NOT_FOUND}' => 'error-404.php'
 ];

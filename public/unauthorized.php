07cc9ec6d1cf0fe4f96ddb03aab79fd9fb7781d0
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-25 22:01:01 +0800

    error pages

--- public/unauthorized.php
+++ public/unauthorized.php
@@ -1,6 +1,8 @@
 <?php
 
-setcookie('_AUTH_ERROR_', 'xUA-401', time()+10, '/');
-header('Location: /', true, 303);
+//setcookie('_AUTH_ERROR_', 'xUA-401', time()+10, '/');
+//header('Location: /', true, 303);
 
-echo "Unauthorized Access";
+//echo "Unauthorized Access";
+
+echo $_SERVER['PHP_AUTH_USER'];

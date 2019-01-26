5121cd89e4853d2879d00fb5ffd5acf136e42247
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-26 21:55:48 +0800

    logout

--- public/unauthorized.php
+++ public/unauthorized.php
@@ -3,3 +3,5 @@
 setcookie('_AUTH_ERROR_', 'xAF', time()+10, '/');
 
 ?>
+<!DOCTYPE html>
+<html><head><meta name="theme-color" content="#3498db"><meta http-equiv="refresh" content="0; URL='/'" /></head><body style="background: #3498db;"></body></html>

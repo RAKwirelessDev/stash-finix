f0f2832f43cb8baa08975526074724c817599513
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-26 08:43:09 +0800

    err

--- public/unauthorized.php
+++ public/unauthorized.php
@@ -4,4 +4,4 @@ setcookie('_AUTH_ERROR_', 'xUA', time()+10, '/');
 
 ?>
 <!DOCTYPE html>
-<html><head><meta http-equiv="refresh" content="0; URL='/'" /></head><body></body></html>
+<html><head><meta http-equiv="refresh" content="0; URL='/'" /></head><body style="background:#3498db;"></body></html>

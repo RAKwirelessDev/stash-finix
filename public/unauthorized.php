7ceab7bb405d85e436c68e31f22913202ce91307
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-26 08:17:47 +0800

    deploy

--- public/unauthorized.php
+++ public/unauthorized.php
@@ -1,9 +1,6 @@
 <?php
 
-//setcookie('_AUTH_ERROR_', 'xUA', time()+10, '/');
-//header('Location: /', true, 303);
+etcookie('_AUTH_ERROR_', 'xUA', time()+10, '/');
+header('Location: /', true, 303);
 
-//echo "Unauthorized Access";
-
-header('Content-Type: text/plain');
-print_r($_SERVER);
+echo "Unauthorized Access";

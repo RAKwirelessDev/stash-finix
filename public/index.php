33f43e3f4bc69b9d8d0f98e210cd210379bc2bce
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-26 16:54:45 +0800

    dev

--- public/index.php
+++ public/index.php
@@ -4,7 +4,8 @@ error_reporting(0);
 require_once('../routes.php');
 require_once('../src/classes/autoload.php');
 
+
+$basic_auth = new Core\BasicAuth('../.htpasswd', true);
 $url_path = Core\Commons::url_path();
 $router = new Core\Router(_ROUTES_);
 $router->fetch($url_path);
-$basic_auth = new Core\BasicAuth('../.htpasswd', true);

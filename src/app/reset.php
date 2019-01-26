26c2c6561b205121a25964d3d65bbf16f50b126e
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-26 20:17:57 +0800

    dev

--- src/app/reset.php
+++ src/app/reset.php
@@ -3,7 +3,7 @@
 $username = $_POST['email'];
 
 if (!empty($username)) {
-    if (in_array($username, $basic_auth->users)) {
+    if (key_exists($username, $basic_auth->users)) {
         $xua = [true, 'Recovery Email Sent'];
     } else {
         $xua = [true, 'User Doesn\'t Exist'];

b00e72f69ad8ecffeb1c422a32782bb3e62ca69b
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-26 08:30:01 +0800

    home

--- public/unauthorized.php
+++ public/unauthorized.php
@@ -1,6 +1,12 @@
 <?php
 
 setcookie('_AUTH_ERROR_', 'xUA', time()+10, '/');
-header('Location: /', true, 303);
 
-echo "Unauthorized Access";
+?>
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="refresh" content="0; URL='/'" /></head>
+<body>
+Redirecting to <a href="/">home</a>
+</body>
+</html>

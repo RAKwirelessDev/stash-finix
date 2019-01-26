d8bd41ea5d343a0927228706ef9b8044ef4e9ee6
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-26 20:15:59 +0800

    dev

--- src/app/reset.php
+++ src/app/reset.php
@@ -1,3 +1,11 @@
 <?php
 
 $username = $_POST['email'];
+
+if (!empty($username)) {
+    if (in_array($username, $basic_auth->users)) {
+
+    } else {
+        $xua = [true, 'User Doesn\'t Exist'];
+    }
+}

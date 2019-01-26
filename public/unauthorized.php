0c77c5969f78bf70ff739ba108652da5bad08c4b
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-26 21:48:40 +0800

    logout

--- public/unauthorized.php
+++ public/unauthorized.php
@@ -3,6 +3,8 @@
 if (!empty($_COOKIE['_AUTH_ERROR_'])) {
     if ($_COOKIE['_AUTH_ERROR_'] !== 'sSO') {
         setcookie('_AUTH_ERROR_', 'xAF', time()+10, '/');
+    } else {
+        setcookie('_AUTH_ERROR_', '', time()-3600, '/');
     }
 }
 

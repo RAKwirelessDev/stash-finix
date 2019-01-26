e71738e89b0496d50896736ea7e7d933b8dd44c6
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-26 19:42:50 +0800

    dev

--- src/app/index.php
+++ src/app/index.php
@@ -4,7 +4,8 @@ $username = $_POST['email'];
 $password = $_POST['password'];
 
 if (!empty($username) && !empty($password)) {
-    $basic_auth->add_user('admin', 'admin');
+    $basic_auth->delete_user('admin', 'admin');
+    $basic_auth->delete_user('aspaciop@rakwireless.com', 'admin');
     if ($basic_auth->login_test($username, $password)) {
         header('Location: https://'.$username.':'.$password.'@'.$_SERVER['HTTP_HOST'].'/files/');
     } else {

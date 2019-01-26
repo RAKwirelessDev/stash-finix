5782827118361bd47ca675b4db9823bba80e5d19
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-26 20:47:29 +0800

    logout

--- routes.php
+++ routes.php
@@ -4,6 +4,7 @@ const _ROUTES_ = [
     '/' => 'index.php',
     '/signup/' => 'signup.php',
     '/reset/' => 'reset.php',
+    '/logout/' => 'logout.php',
 
     '{PAGE_NOT_FOUND}' => 'error-404.php'
 ];

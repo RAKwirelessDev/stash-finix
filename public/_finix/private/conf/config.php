88ff947b780a6716d838d5a0e68c25096931e6eb
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-28 23:54:06 +0800

    pass hash disabled

--- public/_finix/private/conf/config.php
+++ public/_finix/private/conf/config.php
@@ -8,7 +8,7 @@ const _CONFIG_ = [
     SHA512 hash of the info page password, the preset password is the empty string.
     Online hash generator: http://md5hashing.net/hashing/sha512
     */
-    "passhash" => "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e",
+    "passhash" => "DISABLED",
 
     /*
     Resources.
@@ -120,7 +120,7 @@ const _CONFIG_ = [
     */
     "download" => [
         "enabled" => true,
-        "type" => "php-tar",
+        "type" => "shell-zip",
         "packageName" => null,
         "alwaysVisible" => false
     ],
